cb5915156960134c0a4dd4043c32c12c430c43f1
Daniel Tashobya <danieltasho11@gmail.com>
2024-07-14 12:40:35 +0300

    text

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 function App() {
   return (
-    <div><h1>Welcome to our website!</h1></div>
+    <div><h1>Welcome to our website!</h1>
+    <h2>This is my first custom react website project</h2>
+    </div>
   )
 }
 
