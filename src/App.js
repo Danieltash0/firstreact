f041ca13816e733b01b7a21dedd2ea75dff4315d
Daniel Tashobya <danieltasho11@gmail.com>
2024-07-14 14:44:17 +0300

    element

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div><h1>Welcome to our website!</h1>
     <h2>This is my first custom react website project</h2>
+    <p>First p element to test out the positioning</p>
     </div>
   )
 }
